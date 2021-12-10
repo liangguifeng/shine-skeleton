24153a230dcc4fecc898c24350c4addda0fd857c
lianggf01 <lianggf01@mingyuanyun.com>
2021-12-10 17:29:47 +0800

    feat: app register func

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module shine-skeleton
 
 go 1.16
+
+require (
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0
+	google.golang.org/grpc v1.42.0
+)
--- startup/config.go
+++ startup/config.go
@@ -1,17 +1,6 @@
 package startup
 
-import (
-	"git.myscrm.cn/2c/antman-union/vars"
-	"git.myscrm.cn/golang/stark/v4/config"
-)
-
 // LoadConfig 加载配置对象映射
 func LoadConfig() error {
-	config.MapConfig("App", vars.AppSetting, true)
-	config.MapConfig("Messenger", vars.MessengerSetting, true)
-	config.MapConfig("FileBox", vars.FileBoxSetting, true)
-	config.MapConfig("UserCenterAppId", vars.UserCenterAppIdSetting, true)
-	config.MapConfig("OpenApi", vars.OpenApiSetting, true)
-	config.MapConfig("ExtApi", vars.ExtApiSetting, true)
 	return nil
 }
